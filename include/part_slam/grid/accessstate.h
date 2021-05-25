667f4fc29af6fddeaf4b42afd6688e9513c162af
jiamen <2731275645@qq.com>
2021-05-25 14:12:25 +0800

    access state

---
+++ include/part_slam/grid/accessstate.h
@@ -0,0 +1,14 @@
+//
+// Created by zlc on 2021/5/25.
+//
+
+#ifndef _MY_SLAM_GMAPPING_ACCESSSTATE_H_
+#define _MY_SLAM_GMAPPING_ACCESSSTATE_H_
+
+namespace GMapping
+{
+    enum AccessibilityState(Outside=0x0, Inside=0x1, Allocated=0x2);
+};
+
+
+#endif // _MY_SLAM_GMAPPING_ACCESSSTATE_H_
